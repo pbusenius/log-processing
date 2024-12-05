f58cdcc93883898f3b2bca543d1984e47d210699
pbusenius <patrick.busenius@gmail.com>
2024-12-05 15:33:48 +0100

    added tor source

--- tools/update-enrichment-data.py
+++ tools/update-enrichment-data.py
@@ -1,3 +1,12 @@
+import requests
+
+from typing import List
+
+
+TOR_NODES_URL = "https://www.dan.me.uk/torlist/"
+TOR_EXIT_NODES_URL = "https://www.dan.me.uk/torlist/?exit"
+
+
 def get_geo_ip_file():
     pass
 
@@ -6,16 +15,28 @@ def get_asn_ip_file():
     pass
 
 
-def get_tor_nodes_file():
-    pass
+def get_tor_nodes_file() -> List[str]:
+    ip_list = []
+    r = requests.get(TOR_NODES_URL)
+
+    if r.status_code == 200:
+        ip_list = [i for i in r.text.split("\n")]
+
+    return ip_list
 
 
-def get_tor_exit_nodes_file():
-    pass
+def get_tor_exit_nodes_file() -> List[str]:
+    ip_list = []
+    r = requests.get(TOR_EXIT_NODES_URL)
+
+    if r.status_code == 200:
+        ip_list = [i for i in r.text.split("\n")]
+
+    return ip_list
 
 
 def main():
-    pass
+    print(get_tor_exit_nodes_file())
 
 
 if __name__ == "__main__":
