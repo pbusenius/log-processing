bd083e4338abd825beb22b5e12c03ee0a3937637
pbusenius <patrick.busenius@gmail.com>
2024-12-06 13:21:44 +0100

    added polars

--- src/visualization/graph.py
+++ src/visualization/graph.py
@@ -4,7 +4,7 @@ from jaal import Jaal
 from jaal.datasets import load_got
 
 
-def plot_network():
+def plot_network(df: pl.DataFrame=None):
     edge_df, node_df = load_got()
     # init Jaal and run server
     Jaal(edge_df, node_df).plot()
