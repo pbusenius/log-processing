5a7ff67ce840e82a3ecd5cbb0a82833270b4243e
pbusenius <patrick.busenius@gmail.com>
2024-12-06 13:28:48 +0100

    added simple log sources

---
+++ src/source/zeek/conn.py
@@ -0,0 +1,14 @@
+import os
+import polars as pl
+
+from zat.log_to_dataframe import LogToDataFrame
+
+
+def open_conn_log(file: str) -> pl.DataFrame:
+    df = None
+    if os.path.exists(file):
+        df = LogToDataFrame.create_dataframe(file)
+
+        # TODO: apply schema
+
+    return df
