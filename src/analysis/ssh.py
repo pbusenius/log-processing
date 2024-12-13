934fdea8d626281a386fa90b3407ae7dad8c2892
pbusenius <patrick.busenius@gmail.com>
2024-12-13 11:04:31 +0100

    added timeout arg

--- src/analysis/ssh.py
+++ src/analysis/ssh.py
@@ -1,13 +1,11 @@
 import polars as pl
 
-# ts	uid	id.orig_h	id.orig_p	version	auth_success		host_key_alg	host_key
-
 
 def brute_force_detection(
     df: pl.DataFrame, timeout: int = 30, limit: int = 30
 ) -> pl.DataFrame:
     df_brute_force = (
-        df.group_by_dynamic("ts", group_by="id.orig_h", every="30m")
+        df.group_by_dynamic("ts", group_by="id.orig_h", every=f"{timeout}m")
         .agg(
             pl.col("uid").count().alias("number_of_attempts"),
             pl.col("auth_success").n_unique(),
