4518e113a35581a92c4fd23abcaf2efe1fac2f1a
pbusenius <patrick.busenius@gmail.com>
2024-12-16 16:45:56 +0100

    added better regex for all types

--- src/source/os/ssh.py
+++ src/source/os/ssh.py
@@ -2,7 +2,8 @@ import re
 import polars as pl
 
 auth_log_regex = re.compile(
-    "(?P<date>\S*\s*\d*\s\d{2}:\d{2}:\d{2})\s(?P<user>\S*)\s(?P<service>\S*\s)(Accepted (?P<type>password|publickey) for )(?P<remote_user>\S*) from (?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}) port (?P<port>\d*) ssh2(: RSA SHA256:(?P<rsa>\S*))?"
+    # "(?P<date>\\S*\\s*\\d*\\s\\d{2}:\\d{2}:\\d{2})\\s(?P<user>\\S*)\\s(?P<service>\\S*\\s)(Accepted (?P<type>password|publickey) for )(?P<remote_user>\\S*) from (?P<ip>\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}) port (?P<port>\\d*) ssh2(: RSA SHA256:(?P<rsa>\\S*))?"
+    "(?P<date>\\S*\\s*\\d*\\s\\d{2}:\\d{2}:\\d{2})\\s(?P<user>\\S*)\\s(?P<service>\\S*\\s)(?P<type>Disconnected from invalid user (?P<user_name>\\S* )|Invalid user (?P<user_name_2>\\S*) from |Received disconnect from |Accepted publickey for (?P<user_name_3>\\S*) from )(?P<ip>\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}) port (?P<port>\\d*)( ssh2(: RSA (?P<rsa>SHA256:\\S*))?)?"
 )
 
 
@@ -12,11 +13,11 @@ def open_log(file: str) -> pl.DataFrame:
         for line in lines:
             x = auth_log_regex.match(line)
             if x is not None:
-                if x["type"] == "password":
+                if "Accepted" in x["type"]:
                     print(
-                        f"{x["date"]},{x["user"]},{x["type"]},{x["remote_user"]},{x["service"].split(":")[0]},{x["ip"]},{x["port"]}"
+                        f"{x["date"]},{x["user"]},T,{x["service"].split(":")[0]},{x["ip"]},{x["port"]},{x["rsa"]}"
                     )
                 else:
                     print(
-                        f"{x["date"]},{x["user"]},{x["type"]},{x["remote_user"]},{x["service"].split(":")[0]},{x["ip"]},{x["port"]},{x["rsa"]}"
+                        f"{x["date"]},{x["user"]},F,{x["service"].split(":")[0]},{x["ip"]},{x["port"]}"
                     )
