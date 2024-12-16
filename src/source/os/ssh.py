2cdbfb9cc3efea6b4c0d3e352e140e24c258dd90
pbusenius <patrick.busenius@gmail.com>
2024-12-16 14:16:06 +0100

    added ssh os source

---
+++ src/source/os/ssh.py
@@ -0,0 +1,17 @@
+import re
+import polars as pl
+
+auth_log_regex = re.compile(
+    "(?P<date>\S*\s*\d*\s\d{2}:\d{2}:\d{2})\s(?P<user>\S*)\s(?P<service>\S*\s)(Accepted (?P<type>password|publickey) for )(?P<remote_user>\S*) from (?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}) port (?P<port>\d*) ssh2(: RSA SHA256:(?P<rsa>\S*))?"
+)
+
+def open_log(file: str) -> pl.DataFrame:
+    with open(file, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            x = auth_log_regex.match(line)
+            if x is not None:
+                if x["type"] ==  "password":
+                    print(f"{x["date"]},{x["user"]},{x["type"]},{x["remote_user"]},{x["service"].split(":")[0]},{x["ip"]},{x["port"]}")
+                else:
+                    print(f"{x["date"]},{x["user"]},{x["type"]},{x["remote_user"]},{x["service"].split(":")[0]},{x["ip"]},{x["port"]},{x["rsa"]}")
