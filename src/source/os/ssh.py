d2c990259378f0f8bda3e4cce47db6681ac9bb7d
pbusenius <patrick.busenius@gmail.com>
2024-12-16 16:49:06 +0100

    removed unused regex

--- src/source/os/ssh.py
+++ src/source/os/ssh.py
@@ -2,7 +2,6 @@ import re
 import polars as pl
 
 auth_log_regex = re.compile(
-    # "(?P<date>\\S*\\s*\\d*\\s\\d{2}:\\d{2}:\\d{2})\\s(?P<user>\\S*)\\s(?P<service>\\S*\\s)(Accepted (?P<type>password|publickey) for )(?P<remote_user>\\S*) from (?P<ip>\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}) port (?P<port>\\d*) ssh2(: RSA SHA256:(?P<rsa>\\S*))?"
     "(?P<date>\\S*\\s*\\d*\\s\\d{2}:\\d{2}:\\d{2})\\s(?P<user>\\S*)\\s(?P<service>\\S*\\s)(?P<type>Disconnected from invalid user (?P<user_name>\\S* )|Invalid user (?P<user_name_2>\\S*) from |Received disconnect from |Accepted publickey for (?P<user_name_3>\\S*) from )(?P<ip>\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}) port (?P<port>\\d*)( ssh2(: RSA (?P<rsa>SHA256:\\S*))?)?"
 )
 
