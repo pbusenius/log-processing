99329205117073c18ba9aa2a5f8e8e10ec0064d9
pbusenius <patrick.busenius@gmail.com>
2024-12-17 11:18:45 +0100

    ruff format

--- src/enrichment/ip.py
+++ src/enrichment/ip.py
@@ -2,18 +2,15 @@ import polars as pl
 from polars_maxminddb import ip_lookup_city, ip_lookup_country, ip_lookup_asn
 
 
-
 def city_information(df: pl.DataFrame, ip_column: str) -> pl.DataFrame:
-    return df.with_columns(
-        ip_lookup_city(df[ip_column]).alias("city_information")
-    )
+    return df.with_columns(ip_lookup_city(df[ip_column]).alias("city_information"))
+
 
 def country_information(df: pl.DataFrame, ip_column: str) -> pl.DataFrame:
     return df.with_columns(
         ip_lookup_country(df[ip_column]).alias("country_information")
     )
 
+
 def asn_information(df: pl.DataFrame, ip_column: str) -> pl.DataFrame:
-    return df.with_columns(
-        ip_lookup_asn(df[ip_column]).alias("asn_information")
-    )
+    return df.with_columns(ip_lookup_asn(df[ip_column]).alias("asn_information"))
