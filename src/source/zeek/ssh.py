c06b76b3e1d6fcc1eedcd24f8e2d0424c2f32980
pbusenius <patrick.busenius@gmail.com>
2024-12-17 12:37:57 +0100

    added type cast to string

--- src/source/zeek/ssh.py
+++ src/source/zeek/ssh.py
@@ -4,6 +4,23 @@ import polars as pl
 from zat.log_to_dataframe import LogToDataFrame
 
 
+def cast_columns(df: pl.DataFrame) -> pl.DataFrame:
+    return df.with_columns(
+        pl.col("id.orig_h").cast(pl.String),
+        pl.col("id.resp_h").cast(pl.String),
+        pl.col("id.resp_p").cast(pl.String),
+        pl.col("auth_success").cast(pl.Categorical),
+        pl.col("client").cast(pl.Categorical),
+        pl.col("server").cast(pl.Categorical),
+        pl.col("cipher_alg").cast(pl.Categorical),
+        pl.col("mac_alg").cast(pl.Categorical),
+        pl.col("compression_alg").cast(pl.Categorical),
+        pl.col("kex_alg").cast(pl.Categorical),
+        pl.col("host_key_alg").cast(pl.Categorical),
+        pl.col("host_key").cast(pl.Categorical),
+    )
+
+
 def open_log(file: str) -> pl.DataFrame:
     log_to_df = LogToDataFrame()
     df = None
@@ -14,6 +31,8 @@ def open_log(file: str) -> pl.DataFrame:
 
         df = df.sort("ts")
         df = df.drop_nulls("auth_success")
+
+        df = cast_columns(df)
 
         # TODO: apply schema
 
