d4e5f90e8990f06c67aab72daa3cf98c04db67f6
pbusenius <patrick.busenius@gmail.com>
2024-12-17 16:25:11 +0100

    ruff format

--- src/analysis/ip.py
+++ src/analysis/ip.py
@@ -2,7 +2,9 @@ import polars as pl
 import fast_geo_distance
 
 
-def ip_locaton_distance(df: pl.DataFrame, key: str, threshold: int = 10000) -> pl.DataFrame:
+def ip_locaton_distance(
+    df: pl.DataFrame, key: str, threshold: int = 10000
+) -> pl.DataFrame:
     # oder by key
     # shift ordered dfs
     # calculate distance form row-row+1
--- src/enrichment/ip.py
+++ src/enrichment/ip.py
@@ -4,14 +4,22 @@ from polars_maxminddb import ip_lookup_city, ip_lookup_country, ip_lookup_asn
 
 def city_information(df: pl.DataFrame, ip_column: str = "id.resp_h") -> pl.DataFrame:
     print(df)
-    return df.with_columns(ip_lookup_city(df[ip_column], "data/GeoLite2-City.mmdb").alias("city_information"))
+    return df.with_columns(
+        ip_lookup_city(df[ip_column], "data/GeoLite2-City.mmdb").alias(
+            "city_information"
+        )
+    )
 
 
 def country_information(df: pl.DataFrame, ip_column: str = "id.resp_h") -> pl.DataFrame:
     return df.with_columns(
-        ip_lookup_country(df[ip_column], "data/GeoLite2-Country.mmdb").alias("country_information")
+        ip_lookup_country(df[ip_column], "data/GeoLite2-Country.mmdb").alias(
+            "country_information"
+        )
     )
 
 
 def asn_information(df: pl.DataFrame, ip_column: str = "id.resp_h") -> pl.DataFrame:
-    return df.with_columns(ip_lookup_asn(df[ip_column], "data/GeoLite2-ASN.mmdb").alias("asn_information"))
+    return df.with_columns(
+        ip_lookup_asn(df[ip_column], "data/GeoLite2-ASN.mmdb").alias("asn_information")
+    )
