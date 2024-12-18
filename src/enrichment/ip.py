4616eba373e5967742a14fd5780af4c40bbb291b
pbusenius <patrick.busenius@gmail.com>
2024-12-18 18:28:03 +0100

    fixed ip column

--- src/enrichment/ip.py
+++ src/enrichment/ip.py
@@ -2,20 +2,20 @@ import polars as pl
 from polars_maxminddb import ip_lookup_city, ip_lookup_country, ip_lookup_asn, ip_lookup_latitude, ip_lookup_longitude
 
 
-def city_information(df: pl.DataFrame, ip_column: str = "id.resp_h") -> pl.DataFrame:
+def city_information(df: pl.DataFrame, ip_column: str = "id.orig_h") -> pl.DataFrame:
     return df.with_columns(
         ip_lookup_city(df[ip_column], "data/GeoLite2-City.mmdb").alias(
             "city_information"
         )
     )
 
-def location_information(df: pl.DataFrame, ip_column: str = "id.resp_h") -> pl.DataFrame:
+def location_information(df: pl.DataFrame, ip_column: str = "id.orig_h") -> pl.DataFrame:
     return df.with_columns(
         ip_lookup_longitude(df[ip_column], "data/GeoLite2-City.mmdb").alias("longitude"),
         ip_lookup_latitude(df[ip_column], "data/GeoLite2-City.mmdb").alias("latitude")
     )
 
-def country_information(df: pl.DataFrame, ip_column: str = "id.resp_h") -> pl.DataFrame:
+def country_information(df: pl.DataFrame, ip_column: str = "id.orig_h") -> pl.DataFrame:
     return df.with_columns(
         ip_lookup_country(df[ip_column], "data/GeoLite2-Country.mmdb").alias(
             "country_information"
@@ -23,7 +23,7 @@ def country_information(df: pl.DataFrame, ip_column: str = "id.resp_h") -> pl.Da
     )
 
 
-def asn_information(df: pl.DataFrame, ip_column: str = "id.resp_h") -> pl.DataFrame:
+def asn_information(df: pl.DataFrame, ip_column: str = "id.orig_h") -> pl.DataFrame:
     return df.with_columns(
         ip_lookup_asn(df[ip_column], "data/GeoLite2-ASN.mmdb").alias("asn_information")
     )
