8bdae83108250058242c2cc024462f3e952886ee
pbusenius <14203989+pbusenius@users.noreply.github.com>
2024-12-18 21:42:57 +0100

    fixed missing " in regex

--- src/source/os/http.py
+++ src/source/os/http.py
@@ -2,7 +2,7 @@ import re
 import polars as pl
 
 http_log_regex = re.compile(
-    r"(?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}) (?P<unknown>\S*) (?P<unknown2>\S*) \[(?P<date>\d{1,2}/\S*/\d{4}:\d{2}:\d{2}:\d{2} \+\d*)] \"(?P<method>\S*) (?P<uri>\S*) (?P<verson>\S*)\" (?P<status_code>\d*) (?P<request_size>\d*) \"(?P<host>\S*)\" \"(?P<user_agent>.*)\""
+    r"(?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}) (?P<unknown>\S*) (?P<unknown2>\S*) \[(?P<date>\d{1,2}/\S*/\d{4}:\d{2}:\d{2}:\d{2} \+\d*)] \"(?P<method>\S*) (?P<uri>\S*) (?P<verson>\S*)\" (?P<status_code>\d*) (?P<request_size>\S*) \"(?P<host>\S*)\" \"(?P<user_agent>.*)\"?"
 )
 
 
@@ -29,8 +29,13 @@ def open_log(file: str) -> pl.DataFrame:
     with open(file, "r") as file:
         lines = file.readlines()
         for line in lines:
-            x = auth_log_regex.match(line)
+            x = http_log_regex.match(line)
+            try:
+                a = x["ip"]
+                pass
+            except TypeError:
+                print(line)
 
-    df = pl.DataFrame(data)
+    # df = pl.DataFrame(data)
 
-    return df
+    # return df
