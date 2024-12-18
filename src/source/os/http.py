d589e242262f16774175ba522c5568e1e04d9a93
pbusenius <14203989+pbusenius@users.noreply.github.com>
2024-12-18 21:33:16 +0100

    ruff format

--- src/source/os/http.py
+++ src/source/os/http.py
@@ -1,7 +1,10 @@
 import re
 import polars as pl
 
-http_log_regex = re.compile(r"(?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}) (?P<unknown>\S*) (?P<unknown2>\S*) \[(?P<date>\d{1,2}/\S*/\d{4}:\d{2}:\d{2}:\d{2} \+\d*)] \"(?P<method>\S*) (?P<uri>\S*) (?P<verson>\S*)\" (?P<status_code>\d*) (?P<request_size>\d*) \"(?P<host>\S*)\" \"(?P<user_agent>.*)\"")
+http_log_regex = re.compile(
+    r"(?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}) (?P<unknown>\S*) (?P<unknown2>\S*) \[(?P<date>\d{1,2}/\S*/\d{4}:\d{2}:\d{2}:\d{2} \+\d*)] \"(?P<method>\S*) (?P<uri>\S*) (?P<verson>\S*)\" (?P<status_code>\d*) (?P<request_size>\d*) \"(?P<host>\S*)\" \"(?P<user_agent>.*)\""
+)
+
 
 def open_log(file: str) -> pl.DataFrame:
     data = {
@@ -19,14 +22,10 @@ def open_log(file: str) -> pl.DataFrame:
         "status_code": 200,
         "status_msg": "OK",
         "tags": [],
-        "resp_fuids": [
-            "FEEsZS1w0Z0VJIb5x4"
-        ],
-        "resp_mime_types": [
-            "text/plain"
-        ]
+        "resp_fuids": ["FEEsZS1w0Z0VJIb5x4"],
+        "resp_mime_types": ["text/plain"],
     }
-        
+
     with open(file, "r") as file:
         lines = file.readlines()
         for line in lines:
