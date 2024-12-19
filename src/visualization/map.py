79b1aaa6c3a1a20ae3fe3df13c614d7b2de7ecfe
pbusenius <patrick.busenius@gmail.com>
2024-12-19 16:10:56 +0100

    added df to geodesic line

--- src/visualization/map.py
+++ src/visualization/map.py
@@ -29,13 +29,18 @@ def open_in_browser(m: folium.Map):
     m.show_in_browser()
 
 
-def add_line(m: folium.Map):
-    geodesic.Geodesic().add_to(m)
+def add_line(df: pl.DataFrame, m: folium.Map):
+    for row in df.iter_rows(named=True):
+        geodesic.Geodesic(
+            latitude_a=52.3785,
+            longitude_a=4.9000,
+            latitude_b=row["latitude"],
+            longitude_b=row["longitude"]
+        ).add_to(m)
+
+
 
 def points(df: pl.DataFrame, name: str = "map.html"):
-    # read test file for testing
-    # df = pl.read_csv("data/test.csv")
-
     centeroid = compute_centeroid(df)
 
     m = folium.Map(location=centeroid, zoom_start=3)
