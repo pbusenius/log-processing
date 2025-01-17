6e88b1bf045a32a509175182d5c7819b6dacb25f
pbusenius <patrick.busenius@gmail.com>
2025-01-17 14:04:30 +0100

    added iqr

--- src/analysis/http.py
+++ src/analysis/http.py
@@ -1,6 +1,8 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 import polars as pl
 from polars_domain_lookup import is_common_domain
-
 
 
 def get_uncommon_domains(df: pl.DataFrame) -> pl.DataFrame:
@@ -24,6 +26,8 @@ def get_common_domains(df: pl.DataFrame) -> pl.DataFrame:
 
     df = df.filter(pl.col("is_top_domain"))
 
+    df.plot.point(x="length", y="width", color="species")
+
     return df
 
 
@@ -31,14 +35,17 @@ def get_periodic_connection_to_host(df: pl.DataFrame) -> pl.DataFrame:
     min_number_of_connections = 10
     df = df.with_columns(change=pl.col("ts").diff())
 
-    df = df.group_by("id.orig_h").agg(
-        pl.col("change").mean().alias("change_mean"),
-        pl.col("change").count().alias("number_of_connections")
-    ).filter(
-        pl.col("number_of_connections") >= min_number_of_connections
+    df = (
+        df.group_by("id.orig_h")
+        .agg(
+            pl.col("change").mean().alias("change_mean"),
+            pl.col("change").count().alias("number_of_connections"),
+            (pl.col("change").quantile(0.25).sub(pl.col("change").quantile(0.75)))
+            .abs()
+            .alias("iqr"),
+        )
+        .filter(pl.col("number_of_connections") >= min_number_of_connections)
     )
-
-    print(df)
 
     return df
 
@@ -47,11 +54,16 @@ def get_periodic_connection_from_host(df: pl.DataFrame) -> pl.DataFrame:
     min_number_of_connections = 10
     df = df.with_columns(change=pl.col("ts").diff())
 
-    df = df.group_by("id.resp_h").agg(
-        pl.col("change").mean().alias("change_mean"),
-        pl.col("change").count().alias("number_of_connections")
-    ).filter(
-        pl.col("number_of_connections") >= min_number_of_connections
+    df = (
+        df.group_by("id.resp_h")
+        .agg(
+            pl.col("change").mean().alias("change_mean"),
+            pl.col("change").count().alias("number_of_connections"),
+            (pl.col("change").quantile(0.25).sub(pl.col("change").quantile(0.75)))
+            .abs()
+            .alias("iqr"),
+        )
+        .filter(pl.col("number_of_connections") >= min_number_of_connections)
     )
 
     return df
