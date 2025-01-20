0b9c18e38ea6c3be9edf2684c61225e567ceec2e
pbusenius <patrick.busenius@gmail.com>
2025-01-20 12:00:43 +0100

    removed unused aggregation

--- src/analysis/ssh.py
+++ src/analysis/ssh.py
@@ -8,7 +8,6 @@ def brute_force_detection(
         df.group_by_dynamic("ts", group_by="id.orig_h", every=f"{timeout}m")
         .agg(
             pl.col("auth_success").count().alias("number_of_attempts"),
-            pl.col("auth_success").n_unique(),
             pl.col(
                 "id.resp_h",
                 "id.resp_p",
