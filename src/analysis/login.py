19811b07198eb85b3e537d4a5c3de2c16bd49b09
pbusenius <patrick.busenius@gmail.com>
2025-01-20 14:14:09 +0100

    changed code order

--- src/analysis/login.py
+++ src/analysis/login.py
@@ -1,3 +1,4 @@
+import random
 import polars as pl
 import fast_geo_distance
 from src.enrichment import ip
@@ -6,11 +7,16 @@ from src.enrichment import ip
 def detect_impossible_travel(df: pl.DataFrame, speed_threshold: int) -> pl.DataFrame:
     df = ip.location_information(df)
 
-    df = (
-        df.group_by("id.orig_h")
-        .map_groups(lambda ip_group: process_ip_group(ip_group))
-        .filter(pl.col("distance") >= speed_threshold)
+    # process every ip
+    df = df.group_by("id.orig_h").map_groups(
+        lambda ip_group: process_ip_group(ip_group)
     )
+
+    # distance threshold
+    df = df.filter(pl.col("distance") >= speed_threshold)
+
+    # extract 
+    df = df.group_by("id.orig_h").agg(pl.col("ts").first(), pl.col("distance").max())
 
     return df
 
@@ -19,6 +25,7 @@ def process_ip_group(df: pl.DataFrame) -> pl.DataFrame:
     df = df.with_columns(
         pl.col("latitude").shift().alias("prev_latitude"),
         pl.col("longitude").shift().alias("prev_longitude"),
+        pl.col("ts").shift().alias("prev_ts"),
     ).with_columns(
         pl.struct(["latitude", "longitude", "prev_longitude", "prev_latitude"])
         .map_elements(
@@ -37,5 +44,6 @@ def calculate_distance(lat_a: float, lon_a: float, lat_b: float, lon_b: float) -
     distance = 0.0
     if lat_b is not None and lon_b is not None:
         distance = fast_geo_distance.geodesic(lat_a, lon_a, lat_b, lon_b)
+        distance = float(random.randint(0, 100))
 
     return distance
