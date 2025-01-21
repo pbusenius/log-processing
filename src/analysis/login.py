6cfd4d0fb7b99ca342e3e31f2e4432a36f9da0ea
pbusenius <patrick.busenius@gmail.com>
2025-01-21 09:59:54 +0100

    added polars-geodesic-distance plugin

--- src/analysis/login.py
+++ src/analysis/login.py
@@ -1,22 +1,23 @@
-import random
 import polars as pl
 import fast_geo_distance
+
 from src.enrichment import ip
+from polars_distance import distance
 
 
-def detect_impossible_travel(df: pl.DataFrame, speed_threshold: int) -> pl.DataFrame:
+def detect_impossible_travel(
+    df: pl.DataFrame, speed_threshold: int, by: str = "id.resp_h"
+) -> pl.DataFrame:
     df = ip.location_information(df)
 
     # process every ip
-    df = df.group_by("id.orig_h").map_groups(
-        lambda ip_group: process_ip_group(ip_group)
-    )
+    df = df.group_by(by).map_groups(lambda ip_group: process_ip_group(ip_group))
 
     # distance threshold
     df = df.filter(pl.col("distance") >= speed_threshold)
 
     # extract
-    df = df.group_by("id.orig_h").agg(
+    df = df.group_by(by).agg(
         pl.col("ts").first(),
         pl.col("distance").mean().alias("mean_distance"),
         pl.col("distance").max().alias("max_distance"),
@@ -26,20 +27,18 @@ def detect_impossible_travel(df: pl.DataFrame, speed_threshold: int) -> pl.DataF
 
 
 def process_ip_group(df: pl.DataFrame) -> pl.DataFrame:
-    df = df.with_columns(
-        pl.col("latitude").shift().alias("prev_latitude"),
-        pl.col("longitude").shift().alias("prev_longitude"),
-        pl.col("ts").shift().alias("prev_ts"),
-    ).with_columns(
-        # TODO: build custom rust plugin
-        pl.struct(["latitude", "longitude", "prev_longitude", "prev_latitude"])
-        .map_elements(
-            lambda x: calculate_distance(
-                x["latitude"], x["longitude"], x["prev_latitude"], x["prev_longitude"]
-            ),
-            return_dtype=pl.Float64,
+    df = (
+        df.with_columns(
+            pl.col("latitude").shift().alias("prev_latitude"),
+            pl.col("longitude").shift().alias("prev_longitude"),
+            pl.col("ts").shift().alias("prev_ts"),
         )
-        .alias("distance")
+        .drop_nans(["prev_latitude", "prev_longitude"])
+        .with_columns(
+            distance("latitude", "longitude", "prev_latitude", "prev_longitude").alias(
+                "distance"
+            )
+        )
     )
 
     return df
@@ -50,6 +49,6 @@ def calculate_distance(lat_a: float, lon_a: float, lat_b: float, lon_b: float) -
     if lat_b is not None and lon_b is not None:
         distance = fast_geo_distance.geodesic(lat_a, lon_a, lat_b, lon_b)
         # ONLY FOR TESTS!!!
-        distance = float(random.randint(0, 100))
+        # distance = float(random.randint(0, 100))
 
     return distance
