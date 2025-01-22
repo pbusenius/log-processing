b9601d94675dc580b15f52c1b84a413cca719b41
pbusenius <patrick.busenius@gmail.com>
2025-01-22 14:07:35 +0100

    refactored df aggregation and timestamp conversion

--- src/visualization/timeline.py
+++ src/visualization/timeline.py
@@ -33,21 +33,24 @@ def plot_data_transfer_over_time(df: pl.DataFrame, out_file: str, y_lim):
 
 
 def plot_conn_transfer_over_time(df: pl.DataFrame, out_file: str, y_lim):
-    df = df.group_by_dynamic("ts",every="1m").agg(
-        pl.col("orig_ip_bytes").sum() / 1e-9,
-        pl.col("resp_ip_bytes").sum() / 1e-9,
-    )
-
-    df = df.with_columns(
-        pl.col("ts").dt.to_string("%d.%m-%H:%M").alias("timestamp_string"),
+    df = (
+        df.group_by_dynamic("ts", every="1m")
+        .agg(
+            pl.col("orig_ip_bytes").sum() / 1e-9,
+            pl.col("resp_ip_bytes").sum() / 1e-9,
+        )
+        .with_columns(
+            pl.col("ts").dt.to_string("%d.%m-%H:%M").alias("timestamp_string")
+        )
     )
 
     df = df["timestamp_string", "orig_ip_bytes", "resp_ip_bytes"]
+
     # add orig_ip line
     ax = sns.lineplot(
         df,
         x="timestamp_string",
-        y="orig_ip_bytes", 
+        y="orig_ip_bytes",
     )
 
     # add resp_ip line
@@ -64,6 +67,3 @@ def plot_conn_transfer_over_time(df: pl.DataFrame, out_file: str, y_lim):
     ax.set(ylabel="Datentransfer GB/h", xlabel="Zeitstempel")
 
     plt.show()
-
-    # plt.savefig(out_file, dpi=1000)
-    # plt.clf()
